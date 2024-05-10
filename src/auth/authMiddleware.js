20a277f34f480612a443e8f0ff0b4ce98e2379b2
Catati64 <catati64@hotmail.com>
2024-05-09 22:29:54 -0600

    Initi proyect

---
+++ src/auth/authMiddleware.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
+const authenticateToken = (res, req, next) => {
+    const token = req.headers['authorization']
+    if(!token){
+        return res.status(401).json({
+            message: 'Unathorized'
+        })
+    }
+
+    jwt.verify(token, process.env.SECRET, (err, user) => {
+        if (err) {
+            return res.status(403).json({
+                message: 'Forbbiden'
+            })
+        }
+        req.user = user;
+        next()
+    })
+}
+
+module.exports = authenticateToken
